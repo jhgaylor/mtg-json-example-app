37c3d2cd1081169ebd225a933445e933143d9f3e
Jake Gaylor <jhgaylor@gmail.com>
2015-08-30 21:38:18 -0600

    remove dumb

--- json-example-app.js
+++ json-example-app.js
@@ -1,8 +1,6 @@
 if (Meteor.isServer) {
-  // var mtgjson = Npm.require('mtgjson');
-
   MTGjson(function(err, data) {
-      if (err) { return console.log(err) };
-      console.log(data.LEA.cards); // Prints out all cards from the Limited Edition Alpha (LEA) set
+    if (err) { return console.log(err) };
+    console.log(data.LEA.cards); // Prints out all cards from the Limited Edition Alpha (LEA) set
   });
 }
